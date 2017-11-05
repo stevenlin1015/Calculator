51480f47182119384594e0a010348f70669fb230
stevenlin1015 <hsuehshan@hotmail.com>
2017-11-06 00:20:57 +0800

    show digit on the display

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -9,8 +9,20 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var display: UILabel!
 
+    var userIsInTheMiddleOfTyping = false
+    
     @IBAction func touchDigit(_ sender: UIButton) {
+        let digit = sender.currentTitle!
+        
+        if userIsInTheMiddleOfTyping {
+            let textCurrentlyInDisplay = display.text!
+            display.text = textCurrentlyInDisplay + digit
+        } else {
+            display!.text = digit
+            userIsInTheMiddleOfTyping = true
+        }
     }
     
 
